e014ce32f4d37d2ff9cd7acf03aa980b974dcc8d
isaac yule <isaac.k.yule@gmail.com>
2017-11-08 11:53:28 +0000

    added adding a new restaurant + validations

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,3 +1,6 @@
 class Restaurant < ApplicationRecord
   validates :name, :address, :category, presence: true
+  validates :category, inclusion: { in: ["chinese", "italian", "japanese", "french", "belgian"] }
+  has_many :reviews
 end
+
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,6 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+Restaurant.create(name: "Dishoom", address: "1 Dishooom Lane", category: "chinese", phone_number: "07585775518")
+Restaurant.create(name: "Yo Sushi", address: "1 Yo Sushi avenue", category: "japanese", phone_number: "0123456778")
+Restaurant.create(name: "Pizza Party", address: "Pizza Party Lane", category: "italian", phone_number: "0123445645")
