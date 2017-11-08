054ae70d7fe78051e63688e16103ccf1abfa8a64
isaac yule <isaac.k.yule@gmail.com>
2017-11-08 12:30:12 +0000

    added reviews, review controller, model, views

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,6 @@
 class Review < ApplicationRecord
+  validates :content, :rating, presence: true
+  validates :rating, numericality: { only_integer: true}
+  validates :rating, inclusion: { in: [1,2,3,4,5] }
   belongs_to :restaurant
 end
